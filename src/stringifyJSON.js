593a55d8a8a8a9d7086fb043fc96eada921a11f6
Muhammad Meigooni <mmeigooni@gmail.com>
2020-08-17 17:18:10 -0600

    Complete stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,34 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (obj === null) {
+    return 'null';
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (typeof obj === 'object') {
+    const results = [];
+    let joinedResults;
+    if (Array.isArray(obj)) {
+      obj.forEach(function(element) {
+        if (!(typeof element === 'function') && !(element === undefined)) {
+          results.push(stringifyJSON(element));
+        }
+      });
+
+      joinedResults = '[' + results.join(',') + ']';
+    } else {
+      for (var key in obj) {
+        if (!(typeof obj[key] === 'function') && !(obj[key] === undefined)) {
+          results.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+        }
+      }
+      joinedResults = '{' + results.join(',') + '}';
+    }
+    return joinedResults;
+  }
+  // base case
+  return '' + obj;
 };
